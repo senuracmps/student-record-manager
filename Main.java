69e80c0afed30fc17fcf68ee7489740145a62ea7
senuracmps@gmail.com <senuracmps@gmail.com>
2026-05-04 14:08:56 +0530

    Implemented addStudent feature

--- Main.java
+++ Main.java
@@ -3,5 +3,12 @@ public class Main {
  static ArrayList<String> students = new ArrayList<>();
  public static void main(String[] args) {
  System.out.println("Student Record Manager");
+ addStudent("Alice");
+ addStudent("Bob");
  }
+ static void addStudent(String name) {
+ students.add(name);
+ System.out.println(name + " added.");
+ }
+
 }
