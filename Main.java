37a08b7351710833cf324b90aafd6f23282f08da
senuracmps@gmail.com <senuracmps@gmail.com>
2026-05-04 14:46:06 +0530

    Implemented searchStudent feature

--- Main.java
+++ Main.java
@@ -8,25 +8,45 @@ public class Main {
  viewStudents();
  deleteStudent("Alice");
  countStudents();
- 
+ searchStudent("alice");
  }
+
  static void addStudent(String name) {
  students.add(name);
  System.out.println(name + " added.");
  }
+
  static void viewStudents() {
  System.out.println("Student List:");
  for (String s : students) {
  System.out.println(s);
+    }
  }
- 
- }
+
  static void deleteStudent(String name) {
  students.remove(name);
  System.out.println(name + " removed.");
  }
+
  static void countStudents() {
  System.out.println("Total students: " + students.size());
+ }
+
+ static void searchStudent(String name) {
+    boolean found = false;
+
+    for (String s : students) {
+        if (s.equalsIgnoreCase(name)) {
+            found = true;
+            break;
+        }
+    }
+
+    if (found) {
+        System.out.println(name + " found.");
+    } else {
+        System.out.println(name + " not found.");
+    }
  }
 
 
